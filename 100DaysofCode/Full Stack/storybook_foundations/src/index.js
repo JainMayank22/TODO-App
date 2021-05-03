8791afe7dca9cc15bf430ee218df1325f5fc6b95
Mayank Jain <maxjain71@gmail.com>
2021-05-02 22:50:28 -0500

    Design System using React and Storybook

---
+++ 100DaysofCode/Full Stack/storybook_foundations/src/index.js
@@ -0,0 +1,50 @@
+import React, {useState} from "react";
+import ReactDOM from "react-dom";
+import PrimaryButton, {
+  SecondaryButton,
+  TertiaryButton
+} from "./components/Buttons";
+import { GlobalStyle, darkTheme, defaultTheme } from "./utils";
+import { ThemeProvider } from "styled-components";
+
+
+
+
+const App = () => {
+    const [useDarkTheme, setUseDarkTheme] = useState(false);
+    return (
+      <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+        <button
+          style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+          onClick={() => setUseDarkTheme(true)}
+        >
+          Dark theme
+        </button>
+        <button
+          style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+          onClick={() => setUseDarkTheme(false)}
+        >
+          Default theme
+        </button>
+        <div
+          style={{
+            background: useDarkTheme
+              ? defaultTheme.primaryColor
+              : darkTheme.primaryColor,
+            width: "100vw",
+            height: "90vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around"
+          }}
+        >
+          <PrimaryButton modifiers={["small"]}>Hello World</PrimaryButton>
+          <SecondaryButton modifiers={["large"]}>Goodbye World</SecondaryButton>
+          <TertiaryButton>Hey</TertiaryButton>
+          <GlobalStyle />
+        </div>
+      </ThemeProvider>
+    );
+  };
+
+ReactDOM.render(<App />, document.querySelector("#root"));
